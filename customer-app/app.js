5a0dbb5fce1adb225cf648d5174c74c4d5a8cb76
Kenmochi Hiroki <e225408@ie.u-ryukyu.ac.jp>
2026-04-16 18:23:48 +0900

    Bump customer app cache version for photo timeline

--- customer-app/app.js
+++ customer-app/app.js
@@ -12,9 +12,9 @@ const PHOTO_JPEG_QUALITY = 0.74;
 const RESPONSE_EDIT_WINDOW_MS = 24 * 60 * 60 * 1000;
 const BIJIRIS_NEW_BADGE_DAYS = 7;
 const BIJIRIS_HISTORY_LIMIT = 8;
-const APP_VERSION = "20260416-09";
+const APP_VERSION = "20260416-10";
 const CACHE_PREFIX = "mayumi-customer-survey-";
-const ACTIVE_CACHE_NAME = "mayumi-customer-survey-v70";
+const ACTIVE_CACHE_NAME = "mayumi-customer-survey-v71";
 const AUTO_CACHE_MAINTENANCE_INTERVAL_MS = 6 * 60 * 60 * 1000;
 const AUTO_CACHE_MAINTENANCE_KEY = "mayumi_customer_cache_maintenance_at";
 const DEFAULT_ONESIGNAL_APP_ID = "88023099-c99e-44c6-9f7c-2ef08d363768";
@@ -5370,7 +5370,7 @@ function setupInstall() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("./sw.js?v=20260416-09", { updateViaCache: "none" })
+        .register("./sw.js?v=20260416-10", { updateViaCache: "none" })
         .then((registration) => {
           const activateWaiting = () => {
             registration.waiting?.postMessage({ type: "SKIP_WAITING" });
--- customer-app/sw.js
+++ customer-app/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "mayumi-customer-survey-v70";
-const ASSET_VERSION = "20260416-09";
+const CACHE_NAME = "mayumi-customer-survey-v71";
+const ASSET_VERSION = "20260416-10";
 const APP_ASSETS = [
   "./",
   "./index.html",
